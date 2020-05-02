8351b88d03b86032e4f8ed8c026bd270453c1f51
Albert Anthony <albert.anthony1999@gmail.com>
2020-05-02 08:30:50 -0500

    Added sorting feature to API by using mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,16 +5,24 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
     try{
         // BUILD QUERY
-        // 1) Filtering
+        // 1A) Filtering
         const queryObj = { ...req.query }; // Must be a copy of the object in order to not alter the original object
         const excludeFields = ['page', 'sort', 'limit', 'fields'];
         excludeFields.forEach(el => delete queryObj[el]);
 
-        // 2) Advanced Filtering
+        // 1B) Advanced Filtering
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); // \b so that it only matches with the letters we want and not words that contain such letters. /g so that it goes through all occrances and not only one
 
-        const query = Tour.find(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // 2) SORTING
+        if(req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
 
         // EXECUTE QUERY
         const tours = await query;
